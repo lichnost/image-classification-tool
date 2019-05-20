3bf1c64d5bf20b01f9c00f348cd41203bb20198c
Pavel Semenov <otlichnosti@gmail.com>
2019-05-20 14:39:02 +0300

    Multiply classes support

--- index.js
+++ index.js
@@ -9,17 +9,19 @@ const sqlite3 = require('sqlite3');
 let db = new sqlite3.Database('images.db');
 
 async function setup(){
-    await db.run('CREATE TABLE IF NOT EXISTS images (name TEXT UNIQUE, class TEXT, classified BOOLEAN);', (result, err)=> {
+    await db.run('CREATE TABLE IF NOT EXISTS images (name TEXT UNIQUE, classes TEXT, complete DATE, classified BOOLEAN);', (result, err)=> {
         if (err){
             throw err;
         }
         else {
+            console.log('Initialize images');
             fs.readdir('./images', (err, files) => {
                 if (err){
                     throw err;
                 }
                 else {
                     for (let file of files){
+                        console.log(file);
                         db.run('INSERT INTO images (name, classified) VALUES (?, ?)', [file, false], (result, err) => {
                             if(err){
                                 console.log(`DB Initialization Error: ${err}`)
@@ -58,13 +60,51 @@ app.get('/', (req, res) => {
     });
 })
 
-app.post('/:image/:className', (req, res) =>{
+app.post('classify/:action/:image/:className', (req, res) => {
     console.log(req.params.image + ":" + req.params.className);
+    var image = req.params.image;
+    var className = req.params.className;
+    var add = req.params.action.localeCompare('add');
+    db.get('SELECT classes FROM images WHERE name = ?', [image], (err, result) => {
+        if (err) {
+            res.send(err);
+        } else if (result) {
+            var classes = JSON.parse(result['classes']);
+            if (classes && Array.isArray(classes)) {
+                classes = new Array(0);                
+            }
+            var index = classes.findIndex((value) => className.localeCompare(value));
+            if (add && index == -1) {
+                classes.push([className]);
+            } else if (!add && index != -1) {
+                classes = classes.filter((value) => !className.localeCompare(value));
+            }
 
-    db.run('UPDATE images SET class = ?, classified = ? WHERE name = ?',
-    [req.params.className, true, req.params.image]);
+            db.run('UPDATE images SET classes = ? WHERE name = ?', [JSON.stringify(classes), req.params.image]);
+        }
+    });
+});
 
-    res.redirect('/');
+app.get('/next/:image', (req, res) =>{
+    console.log(req.params.image + ":" + req.params.className);
+    var image = req.params.image;
+    var classes = new Array(0);
+    var query = req.query;
+    for (var property in query) {
+        if (query.hasOwnProperty(property)) {
+            classes.push(property);
+        }
+    }
+    db.run('UPDATE images SET classes = ?, classified = ?, complete = ? WHERE name = ?',
+            [JSON.stringify(classes), true, new Date(), image],
+            (result, err) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.redirect('/');
+        }
+    });
+    
 });
 
 
